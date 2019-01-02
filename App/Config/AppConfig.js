66eae71c39438cdbf000316c932f8b73b44746d5
Mohamed Elmi <maxto024@gmail.com>
2019-01-02 15:11:42 +0300

    V1

--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -1,8 +1,7 @@
 // Simple React Native specific changes
-import {Platform} from 'react-native'
-;
+
 
 export default {
-  apiUrl: Platform.OS === 'ios' ? 'https://somaliapp.com:8080' : 'http://somaliapp.com:8080',
+  apiUrl: 'https://somaliapp.com:8080',
   appUrlScheme: 'easypos',
 };
